9b64f87bad98d972cbc084986141750b2f7c085c
rkk <zhhailon@gmail.com>
2017-10-24 16:08:14 -0400

    in the middle

--- demo/App/BufferOverflow/Mem.cpp
+++ demo/App/BufferOverflow/Mem.cpp
@@ -1,13 +1,28 @@
 #include "../App.h"
 #include "Enclave_u.h"
+#include <cstring>
+#include <inttypes.h>
+#include <unistd.h>
 
 // hailong:
 void ecall_mem_functions(void)
 {
-    char buf[20];
-    for(int i = 0; i < 20; i++)
-    {
-        buf[i] = i;
+    uint64_t buf[30];
+    memset(buf, 0xff, 30 * sizeof(uint64_t));
+    for (uint64_t i = 0x7ffff5000000;
+            i <= 0x7ffff502b000;
+            i++) {
+        buf[3] = i;
+        /* printf("%p buf[ 0]=0x%" PRIx64 "\n", buf, i); */
+        for (int j = 4; j < 30; j++) {
+            buf[j] = -1;
+            /* printf("%p buf[%2d]=0x%" PRIx64 "\n", &buf[j], j, buf[j]); */
+        }
+
+        printf("PID=%d\n", getpid());
+        printf("Enter a character to enter enclave...\n");
+        getchar();
+
+        ecall_memcpy_in(global_eid, buf, 4);
     }
-    ecall_memcpy_in(global_eid, buf, 20);
 }
--- demo/Enclave/BufferOverflow/Mem.cpp
+++ demo/Enclave/BufferOverflow/Mem.cpp
@@ -3,16 +3,19 @@
 #include "sgx_trts.h"
 #include "../Enclave.h"
 #include "Enclave_t.h"
+#include <inttypes.h>
 
 // hailong:
-void ecall_memcpy_in(const char *mem, size_t len)
+void ecall_memcpy_in(const uint64_t *mem, size_t len)
 {
-    char buf[10];
-    memcpy(buf, mem, len);
+    uint64_t buf[1];
+    /* for(int i = 0; i < len; ++i) */
+    /* { */
+    /*     printf("[%3d] %p: 0x%" PRIx64 "\n", i, &mem[i], mem[i]); */
+    /* } */
+    memcpy(buf, mem, len * sizeof(uint64_t));
     for(int i = 0; i < len; ++i)
     {
-        printf("[%3d] %p: 0x%02hhx\n", i, &buf[i], buf[i]);
+        printf("[%3d] %p: 0x%" PRIx64 "\n", i, &buf[i], buf[i]);
     }
-
 }
-
