3f203dd4cccda5d21958afab981d931016dff469
rkk <zhhailon@gmail.com>
2017-10-25 13:44:44 -0400

    disable stack protection & add cr2 module

--- demo/Enclave/BufferOverflow/Mem.cpp
+++ demo/Enclave/BufferOverflow/Mem.cpp
@@ -9,10 +9,6 @@
 void ecall_memcpy_in(const uint64_t *mem, size_t len)
 {
     uint64_t buf[1];
-    /* for(int i = 0; i < len; ++i) */
-    /* { */
-    /*     printf("[%3d] %p: 0x%" PRIx64 "\n", i, &mem[i], mem[i]); */
-    /* } */
     memcpy(buf, mem, len * sizeof(uint64_t));
     for(int i = 0; i < len; ++i)
     {
