5e10093aff21ff6120da4f0c35da9b9d53cb2690
rkk <zhhailon@gmail.com>
2017-11-28 22:48:31 -0500

    update

--- demo/Enclave/ROP/rop.cpp
+++ demo/Enclave/ROP/rop.cpp
@@ -1,39 +1,51 @@
 #include "../Enclave.h"
+#include <stdint.h>
+
+void foo() {
+    uint64_t i = 2;
+    asm("movq %0, %%rbx" :: "r"(i));
+    register int myrbx asm("rbx");
+    printf("rbx = 0x%x\n", myrbx);
+}
 
 #if defined(__cplusplus)
 extern "C" {
 #endif
 
-	// rdi: 1: rax, 2: rbx, 0: others
-	int which_reg = 0;
-	char *shadowstack = 0;
-	char *shadowrsp = 0;
+    // rdi: 1: rax, 2: rbx, 0: others
+    int which_reg = 0;
+    char *shadowstack = 0;
+    char *shadowrsp = 0;
 
-	void checkheapexistence()
-	{
-		if(shadowstack == 0)
-		{
-			shadowstack = (char*)malloc(1000);
-			shadowrsp = shadowstack;
-		}
-	}
+    void checkheapexistence()
+    {
+        if(shadowstack == 0)
+        {
+            shadowstack = (char*)malloc(1000);
+            shadowrsp = shadowstack;
+        }
+    }
 
-	void checkrsp()
-	{
+    void checkrsp()
+    {
 
-	}
+    }
 
-	void heap_stack_push(int a) {
+    void heap_stack_push(int a) {
         printf("push\n");
-        printf("%x\n", a);
-		checkheapexistence();
-	}
+        register int myrsp asm("rsp");
+        printf("0x%x - 0x%x = 0x%x\n", a, myrsp, a - myrsp);
+        checkheapexistence();
+    }
 
-	void heap_stack_pop() {
+    void heap_stack_pop(int a) {
         printf("pop\n");
-		checkheapexistence();
-		checkrsp();
-	}
+        register int myrsp asm("rsp");
+        printf("0x%x - 0x%x = 0x%x\n", a, myrsp, a - myrsp);
+        checkheapexistence();
+        checkrsp();
+        foo();
+    }
 
 #if defined(__cplusplus)
 }
